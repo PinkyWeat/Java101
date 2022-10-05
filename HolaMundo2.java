945c78bcd1213c1a38b8669ed26f978785c6b4bd
Denisse Landau <105326065+PinkyWeat@users.noreply.github.com>
2022-10-05 19:14:30 -0300

    string sout

--- HolaMundo2.java
+++ HolaMundo2.java
@@ -2,5 +2,14 @@ public class HolaMundo2 {
     public static void main(String args[]) {
         int entero = 25;
         System.out.println(entero);
+        //Modificando el valor de la variable
+        entero = 420;
+        System.out.println(entero);
+
+        String miString = "Queen Denisse";
+        System.out.println(miString);
+
+        miString = "Queen Denisse :D";
+        System.out.println(miString);
     }
 }
