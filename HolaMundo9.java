b2f4bc92cc989dfc2a985b67d4597214aefecad3
Denisse Landau <105326065+PinkyWeat@users.noreply.github.com>
2022-10-06 19:36:22 -0300

    double

---
+++ HolaMundo9.java
@@ -0,0 +1,8 @@
+public class HolaMundo9 {
+    public static void main(String args[]) {
+        // modo debuggin' xd con Apache NetBeans y yo tengo IntelliJ
+        var numeroEntero = 25.0; // la va a crear de tipo double amenos de que le
+        System.out.println("numeroEntero = " + numeroEntero); // ponga la F.
+        // inferencia de tipos xdxd
+    }
+}
