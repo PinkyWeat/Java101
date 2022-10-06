ba906ad59d2c8bb847124901de128ac01dd0e7f5
Denisse Landau <105326065+PinkyWeat@users.noreply.github.com>
2022-10-06 14:41:57 -0300

    max & min value
    usando Byte.<funciones> y viendo de transformar obligado a una variable, como por ej: (byte) 129

---
+++ HolaMundo6.java
@@ -0,0 +1,14 @@
+public class HolaMundo6 {
+    public static void main(String args[]) {
+        // tipos primitivos Enteros o Flotantes
+        // hoy solo Enteros: byte, short, int, long
+
+        byte numByte = 25; // si le pones 129 salta error
+        System.out.println("valor min numByte = " + Byte.MIN_VALUE); // -128
+        System.out.println("valor max numByte = " + Byte.MAX_VALUE); // 128
+
+        byte numByte2 = (byte) 129; // el (byte) transforma y obliga a que sea byte
+        System.out.println("Valor byte ahora: " + numByte2);
+
+    }
+}
