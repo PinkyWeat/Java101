f69e06c6f54f911f043fdda8ef6a0e63a0ee9ad5
Denisse Landau <105326065+PinkyWeat@users.noreply.github.com>
2022-10-06 21:56:16 -0300

    Conversion de tipos en Java
    Integer.parseInt xej + consola

---
+++ HolaMundo12.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class HolaMundo12 {
+    public static void main(String args[]) {
+        // Conversion de Tipos en Java
+        // string a entero y viceversa
+        var edad = Integer.parseInt("20"); // recibe string y devuelve int
+        System.out.println("edad = " + (edad + 1));
+        
+        var edad1 = "20";
+        System.out.println("(edad1 + 1) = " + (edad1 + 1)); // concatena
+
+        var valorPI = Double.parseDouble("3.1416");
+        System.out.println("valorPI = " + valorPI);
+        System.out.println("(valorPI) + 1 = " + (valorPI) + 1); // retorna 3.14161
+
+        // Pedir un valor
+        var consola = new Scanner(System.in);
+        System.out.println("Proporciona tu edad: ");
+        edad = Integer.parseInt(consola.nextLine());
+        System.out.println("edad = " + edad);
+    }
+}
