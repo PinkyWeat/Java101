3bca709372497146ee89386256d24bada36ebdbe
Denisse Landau <105326065+PinkyWeat@users.noreply.github.com>
2022-10-25 17:14:59 -0300

    Operadores relacionales

---
+++ HolaMundo17.java
@@ -0,0 +1,20 @@
+public class HolaMundo17 {
+    public static void main(String args[]) { // operadores de igualdad & relacionales
+        var a = 3;
+        var b = 2;
+
+        var c = (a == b);
+        System.out.println("resultado a == b: " + c);
+
+        var d = a != b;
+        System.out.println("resultado a != b: " + d);
+
+        var cadena = "hola";
+        var cadena2 = "adios";
+        var e = cadena == cadena2; // compara referencias de objeto!!
+        System.out.println("resultado cadena == cadena2 : " + e);
+
+        var f = !cadena.equals(cadena2); // checkea contenido
+        System.out.println("with equals: " + f);  
+    }
+}
