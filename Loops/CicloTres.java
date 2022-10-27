38aecd5d9900cd121286b9ae7decd7b9cf758779
Denisse Landau <105326065+PinkyWeat@users.noreply.github.com>
2022-10-27 19:52:43 -0300

    for

---
+++ Loops/CicloTres.java
@@ -0,0 +1,7 @@
+public class CicloTres {
+    public static void main(String args[]) {
+        for (var contador = 0; contador < 3; contador++) {
+            System.out.println("contador = " + contador);
+        }
+    }
+}
