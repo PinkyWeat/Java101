cb9372903d1207b2eb4cb4b8511a03deb629cf39
Denisse Landau <105326065+PinkyWeat@users.noreply.github.com>
2022-10-27 19:05:33 -0300

    While

---
+++ Loops/CiclosUno.java
@@ -0,0 +1,10 @@
+public class CiclosUno {
+    public static void main(String args[]) {
+        var contador = 0;
+
+        while (contador < 3) {
+            System.out.println("contador = " + contador);
+            contador++; 
+        }
+    }
+}
